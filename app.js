f4da933f52c0a50fbe507c5e6e6f800afa17fd0f
Deependra Singh <deependra.singh@faircent.com>
2021-06-14 01:11:29 +0530

    second node commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+const cors = require("cors");
 const connectDB = require("./config/db");
 
 connectDB();
@@ -18,6 +19,7 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
