749e9a9527f5716eca10644e1ded25a849b26945
Deependra Singh <deependra.singh@faircent.com>
2021-06-14 13:38:06 +0530

    First Commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require("http-errors");
 var express = require("express");
+var concurrently = require("concurrently");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
@@ -24,7 +25,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client", "build", "index.html")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
