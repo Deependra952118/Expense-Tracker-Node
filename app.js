855846db3f9dbc0aa8697700745fca837842c583
Deependra Singh <deependra.singh@faircent.com>
2021-06-14 14:51:21 +0530

    make it better

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client", "build", "index.html")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
